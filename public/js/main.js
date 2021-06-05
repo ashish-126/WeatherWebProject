05f640d55dfe25d3c3559db272f37c651e320eca
ashish-126 <ashishwankhede96@gmail.com>
2021-06-05 20:31:25 +0530

    updating api url for heroku

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ const getInfo = async(event)=>{
         city_name.innerText =  `Please write the city name before search`;
     }else{
         try{
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=c50e5ced20f429cfd9d441769d6cbe2d`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=c50e5ced20f429cfd9d441769d6cbe2d`;
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
